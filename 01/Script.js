4ca7b1e29030ea3c85b76a7ddc32f1aed2822e59
pegasus <2026808499@qq.com>
2026-04-26 19:15:21 +0800

    修改立方体文件

--- 01/Script.js
+++ 01/Script.js
@@ -7,6 +7,7 @@ const scene = new THREE.Scene();//添加一个新的场景
 const geometry = new THREE.BoxGeometry(1, 1, 1);//创建一个立方体，参数分别是宽、高、深
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });//创建一个材质，参数是一个对象，可以设置颜色
 const mesh = new THREE.Mesh(geometry, material);//创建一个网格，参数是几何体和材质
+mesh.position.set(0.7, -0.6, 1);//设置网格的位置
 scene.add(mesh);//将网格添加到场景
 
 const sizes = {
@@ -24,6 +25,5 @@ const renderer = new THREE.WebGLRenderer({
 });//创建一个渲染器，参数是一个对象，可以设置画布
 
 renderer.setSize(sizes.width, sizes.height);//设置渲染器的尺寸
-
 renderer.render(scene, camera);//渲染场景和相机
 
